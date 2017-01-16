d47a96ee56d39b9d0aa081254a5fbe28743b8566
ftrader <freetrader@tuta.io>
2017-01-16 22:14:17 +0100

    clean up to conform to PEP8
    Best to get these out the way early so we can stay PEP8 compliant.
    This should help avoid the ire of Python programmers :-)
    
    Tools used: pep8, autopep8
    
    (available through pip install on most friendly distros near you)

--- python/runtests.py
+++ python/runtests.py
@@ -29,11 +29,11 @@ class TestCMF(unittest.TestCase):
         self.assertEqual(buffer[1], 177)
         self.assertEqual(buffer[2], 112)
 
-        parser = MessageParser(buffer, 0, 3);
-        self.assertEquals(MessageParser.Type.FoundTag, parser.next());
-        self.assertEquals(15, parser.tag);
-        self.assertEquals(6512, parser.value);
-        self.assertEquals(MessageParser.Type.EndOfDocument, parser.next());
+        parser = MessageParser(buffer, 0, 3)
+        self.assertEquals(MessageParser.Type.FoundTag, parser.next())
+        self.assertEquals(15, parser.tag)
+        self.assertEquals(6512, parser.value)
+        self.assertEquals(MessageParser.Type.EndOfDocument, parser.next())
 
     def test_basic2(self):
         buffer = bytearray(b"000000000000000000000000000000000")
@@ -46,24 +46,24 @@ class TestCMF(unittest.TestCase):
         self.assertEqual(buffer[3], 177)
         self.assertEqual(buffer[4], 112)
 
-        parser = MessageParser(buffer, 0, 5);
-        self.assertEquals(MessageParser.Type.FoundTag, parser.next());
-        self.assertEquals(129, parser.tag);
-        self.assertEquals(6512, parser.value);
-        self.assertEquals(MessageParser.Type.EndOfDocument, parser.next());
+        parser = MessageParser(buffer, 0, 5)
+        self.assertEquals(MessageParser.Type.FoundTag, parser.next())
+        self.assertEquals(129, parser.tag)
+        self.assertEquals(6512, parser.value)
+        self.assertEquals(MessageParser.Type.EndOfDocument, parser.next())
 
     def test_types(self):
         buffer = bytearray(b"000000000000000000000000000000000")
         builder = MessageBuilder(buffer, 0)
-        builder.add_string(1, u"Föo");
-        builder.add_bytes(200, b"hihi");
-        builder.add_bool(3, True);
-        builder.add_bool(40, False);
+        builder.add_string(1, u"Föo")
+        builder.add_bytes(200, b"hihi")
+        builder.add_bool(3, True)
+        builder.add_bool(40, False)
         self.assertEqual(builder.get_position(), 17)
 
         # string '1'
         self.assertEquals(buffer[0], 10)
-        self.assertEquals(buffer[1], 4) # serialized string length
+        self.assertEquals(buffer[1], 4)  # serialized string length
         self.assertEquals(buffer[2], 70)
         self.assertEquals(buffer[3], 195)
         self.assertEquals(buffer[4], 182)
@@ -73,34 +73,33 @@ class TestCMF(unittest.TestCase):
         self.assertEquals(buffer[6], 251)
         self.assertEquals(buffer[7], 128)
         self.assertEquals(buffer[8], 72)
-        self.assertEquals(buffer[9], 4) #length of bytearray
-        self.assertEquals(buffer[10], 104) # 'h'
-        self.assertEquals(buffer[11], 105) # 'i'
-        self.assertEquals(buffer[12], 104) # 'h'
-        self.assertEquals(buffer[13], 105) # 'i'
+        self.assertEquals(buffer[9], 4)  # length of bytearray
+        self.assertEquals(buffer[10], 104)  # 'h'
+        self.assertEquals(buffer[11], 105)  # 'i'
+        self.assertEquals(buffer[12], 104)  # 'h'
+        self.assertEquals(buffer[13], 105)  # 'i'
 
         # bool-true '3'
-        self.assertEquals(buffer[14], 28);
+        self.assertEquals(buffer[14], 28)
 
         # bool-false '40'
-        self.assertEquals(buffer[15], 253);
-        self.assertEquals(buffer[16], 40);
+        self.assertEquals(buffer[15], 253)
+        self.assertEquals(buffer[16], 40)
 
-        parser = MessageParser(buffer, 0, builder.get_position());
-        self.assertEquals(MessageParser.Type.FoundTag, parser.next());
-        self.assertEquals(1, parser.tag);
+        parser = MessageParser(buffer, 0, builder.get_position())
+        self.assertEquals(MessageParser.Type.FoundTag, parser.next())
+        self.assertEquals(1, parser.tag)
         self.assertEquals(u"Föo".encode('utf-8'), parser.string_value())
-        self.assertEquals(MessageParser.Type.FoundTag, parser.next());
-        self.assertEquals(200, parser.tag);
+        self.assertEquals(MessageParser.Type.FoundTag, parser.next())
+        self.assertEquals(200, parser.tag)
         self.assertEquals(b"hihi", parser.string_value())
-        self.assertEquals(MessageParser.Type.FoundTag, parser.next());
-        self.assertEquals(3, parser.tag);
+        self.assertEquals(MessageParser.Type.FoundTag, parser.next())
+        self.assertEquals(3, parser.tag)
         self.assertEquals(True, parser.value)
-        self.assertEquals(MessageParser.Type.FoundTag, parser.next());
-        self.assertEquals(40, parser.tag);
+        self.assertEquals(MessageParser.Type.FoundTag, parser.next())
+        self.assertEquals(40, parser.tag)
         self.assertEquals(False, parser.value)
-        self.assertEquals(MessageParser.Type.EndOfDocument, parser.next());
+        self.assertEquals(MessageParser.Type.EndOfDocument, parser.next())
 
 if __name__ == '__main__':
     unittest.main()
-
